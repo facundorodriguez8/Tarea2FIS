e0ab4417496df6b8efa9de42ef2e588d7ab148e9
Facundo Rodriguez <facundo.rodriguez@isaportal.uy>
2020-04-05 20:38:55 -0300

    Commit B

--- Tarea2/src/dominio/Usuario.java
+++ Tarea2/src/dominio/Usuario.java
@@ -8,5 +8,43 @@ public class Usuario {
 	private int edad;
 	private int cantidadDeHijos;
 	private int saldo = 0;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public int getCantidadDeHijos() {
+		return cantidadDeHijos;
+	}
+	public void setCantidadDeHijos(int cantidadDeHijos) {
+		this.cantidadDeHijos = cantidadDeHijos;
+	}
+	public int getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+	
+	
 	
 }
